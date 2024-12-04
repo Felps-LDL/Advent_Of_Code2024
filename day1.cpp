56563dc32ba9922121674955c4b6457904b3ae52
Felipe Lages de Lima <106702104+Felps-LDL@users.noreply.github.com>
2024-12-04 00:16:19 -0300

    Create day1.cpp

---
+++ day1.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+#define optimize ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl '\n'
+#define MAXN 50010
+#define pii pair<int,int>
+#define piii pair<pair<int,int>, int>;
+#define ll long long
+#define INF 1e9
+#define LLINF 4e18
+#define ALL(X) X.begin(), X.end()
+#define EPS = 1e-9
+#define ALL(X) X.begin(), X.end()
+#define piv pair<int, vector<vector<int>>>
+#define f first
+#define s second
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define PB push_back
+#define pq priority_queue<pair<int, pii>, vector<pair<int, pii>>, greater<pair<int, pii>>>
+
+// Felipe Lages de Lima
+
+using namespace std;
+
+ll a, b;
+vector<ll> l1, l2;
+
+int main()
+{
+    ll soma = 0;
+
+    while(true)
+    {
+        cin >> a >> b;
+        if(a == b && b == 0) break;
+        l1.push_back(a);
+        l2.push_back(b);
+    }
+
+    sort(ALL(l1));
+    sort(ALL(l2));
+
+    for(int i = 0; i < l1.size(); i++) soma += abs(l1[i] - l2[i]);
+
+    cout << soma << endl;
+    
+    return 0;
+}
